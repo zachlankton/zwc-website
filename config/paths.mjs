9f54563a0bfda6255974156bb15aa2b36a9988ac
Zach Lankton <zachlankton@gmail.com>
2022-04-10 00:29:09 +0000

    updating to to remove cloudflare env vars

--- config/paths.mjs
+++ config/paths.mjs
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-const ROOTDIR = process.env.ZSSG_ROOT;
-const SRCDIR = process.env.ZSSG_SRC;
-const OUTDIR = process.env.ZSSG_OUT;
+const ROOTDIR = process.env.ZSSG_ROOT || "";
+const SRCDIR = process.env.ZSSG_SRC || "src";
+const OUTDIR = process.env.ZSSG_OUT || "dist";
 export { ROOTDIR, SRCDIR, OUTDIR };
