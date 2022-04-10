2a2be5741bbd2ff46b2256934ec37719dee58766
Zach Lankton <zachlankton@gmail.com>
2022-04-10 05:29:28 +0000

    fixed another csp issue

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,27 +1,32 @@
-function sidebarOpenClickListener(ev) {
-  if (ev.target.matches("#sidebar a")) toggle_sidebar(ev);
-  if (ev.target.matches("#sidebar *")) return;
-  toggle_sidebar(ev);
-}
-
-function toggle_sidebar(ev) {
-  ev.stopPropagation();
+(function () {
+  const mobileSidebarBtn = document.getElementById("mobile-sidebar-btn");
   const sidebar = document.getElementById("sidebar");
-  const btn = document.getElementById("mobile-sidebar-btn");
 
-  if (sidebar.classList.contains("open")) {
-    btn.style.display = "";
-    document.removeEventListener("click", sidebarOpenClickListener);
-    sidebar.classList.remove("open");
-    setTimeout(() => {
-      sidebar.classList.remove("mobile");
-    }, 200);
-  } else {
-    btn.style.display = "none";
-    document.addEventListener("click", sidebarOpenClickListener);
-    sidebar.classList.add("mobile");
-    setTimeout(() => {
-      sidebar.classList.add("open");
-    }, 10);
+  mobileSidebarBtn.addEventListener("click", toggle_sidebar);
+  function sidebarOpenClickListener(ev) {
+    if (ev.target.matches("#sidebar a")) toggle_sidebar(ev);
+    if (ev.target.matches("#sidebar *")) return;
+    toggle_sidebar(ev);
   }
-}
+
+  function toggle_sidebar(ev) {
+    ev.stopPropagation();
+    const btn = mobileSidebarBtn;
+
+    if (sidebar.classList.contains("open")) {
+      btn.style.display = "";
+      document.removeEventListener("click", sidebarOpenClickListener);
+      sidebar.classList.remove("open");
+      setTimeout(() => {
+        sidebar.classList.remove("mobile");
+      }, 200);
+    } else {
+      btn.style.display = "none";
+      document.addEventListener("click", sidebarOpenClickListener);
+      sidebar.classList.add("mobile");
+      setTimeout(() => {
+        sidebar.classList.add("open");
+      }, 10);
+    }
+  }
+})();
